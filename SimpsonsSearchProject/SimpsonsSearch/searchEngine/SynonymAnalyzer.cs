90cf6c7071fc48ac1167c23a061a6bb8ca3e1b9d
Robin Seidel <robin.seidel@hypservice.de>
2019-07-04 15:35:58 +0200

    [add] adding synonyms is now easier + wordlist for alcohol topic

--- SimpsonsSearchProject/SimpsonsSearch/searchEngine/SynonymAnalyzer.cs
+++ SimpsonsSearchProject/SimpsonsSearch/searchEngine/SynonymAnalyzer.cs
@@ -30,17 +30,38 @@ namespace SimpsonsSearch.searchEngine
         // herausfinden wie man mehr als ein string einer base hinzufügt.. 
         public SynonymMap GetSynonyms()
         {
-            var wordlist1 = "weihnachtsmann";
+            var test = "weihnachtsmann";
             var syn1 = "christmas";
-
-            var wordlist2 = "ostern";
             var syn2 = "easter";
-            
+
+            var alcolDic = new Dictionary<string, string>
+            {
+                { "key1", "abuse" },
+                { "key2", "addiction" },
+                {"key3","alcoholic" },
+                {"key4","alcoholism" },
+                {"key5","bar" },
+                {"key6","beer" },
+                {"key7","wine" },
+                {"key8","cider" },
+                {"key9","duff" },
+                {"key10","brew" },
+                {"key11","brewery" },
+                {"key12","vomit" },
+                {"key13", "underage" }, {"key14","drink" }, {"key15","cocktail" }, {"key16","disorder" }, {"key17","liquer" }
+            };
+
 
             var sBuilder = new SynonymMap.Builder(true);
             
-            sBuilder.Add(new CharsRef(wordlist1), new CharsRef(syn1), true);
-            sBuilder.Add(new CharsRef(wordlist1), new CharsRef(syn2),  true);
+            for (int i = 1;  i <= alcolDic.Count(); i++)
+            {
+                var key = $"key{i}";
+                
+                    sBuilder.Add(new CharsRef("alcohol"), new CharsRef(alcolDic[key]), true);
+                
+            }
+
             var synonyms = sBuilder.Build();
             return synonyms;
         }
---
+++ SimpsonsSearchProject/SimpsonsSearch/searchEngine/topics/AlcoholTopic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lucene.Net.Documents;
+using Lucene.Net.Search;
+using SimpsonsSearch.Services;
+
+namespace SimpsonsSearch.searchEngine.topics
+{
+    public class AlcoholTopic : AdvancedSearchWithSynonyms
+    {
+        public AlcoholTopic(IConversionService conversionService) : base(conversionService)
+        {
+            //suchergebnis bearbeiten sodass es episoden anzeigt indem episoden angezeigt werden die spezifischen topic begriffe besonders viel enthalten 
+        }
+
+        public override SearchResults CompileResults(IndexSearcher searcher, TopDocs topDocs)
+        {
+            return base.CompileResults(searcher, topDocs);
+        }
+    }
+}
