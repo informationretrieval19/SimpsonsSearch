0b0733cea747b6813741b7bf0270ce23aa992e8e
rs92vegi <robin.seidel@hypservice.de>
2019-08-14 10:24:38 +0200

    initialised dictionaries

--- SimpsonsSearchProject/SimpsonsSearch/searchEngine/SynonymAnalyzer.cs
+++ SimpsonsSearchProject/SimpsonsSearch/searchEngine/SynonymAnalyzer.cs
@@ -115,6 +115,13 @@ namespace SimpsonsSearch.searchEngine
 
             }
 
+            for (int i = 1; i <= dictionaries.foodDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("food"), new CharsRef(dictionaries.foodDic[key]), true);
+
+            }
             for (int i = 1; i <= dictionaries.fantasyDic.Count(); i++)
             {
                 var key = $"key{i}";
@@ -122,7 +129,153 @@ namespace SimpsonsSearch.searchEngine
                 sBuilder.Add(new CharsRef("fantasy"), new CharsRef(dictionaries.fantasyDic[key]), true);
 
             }
+            for (int i = 1; i <= dictionaries.getting_marriedDic.Count(); i++)
+            {
+                var key = $"key{i}";
 
+                sBuilder.Add(new CharsRef("getting married"), new CharsRef(dictionaries.getting_marriedDic[key]), true);
+
+            }
+            for (int i = 1; i <= dictionaries.holidayDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("holiday"), new CharsRef(dictionaries.holidayDic[key]), true);
+
+            }
+            for (int i = 1; i <= dictionaries.insanityDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("insanity"), new CharsRef(dictionaries.insanityDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.internetDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("internet"), new CharsRef(dictionaries.internetDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.kidsDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("kids"), new CharsRef(dictionaries.kidsDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.lgtbqDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("lgtbq"), new CharsRef(dictionaries.lgtbqDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.mafiaDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("mafia"), new CharsRef(dictionaries.mafiaDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.medical_themedDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("medical"), new CharsRef(dictionaries.medical_themedDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.militaryDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("military"), new CharsRef(dictionaries.militaryDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.moviesDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("movie"), new CharsRef(dictionaries.moviesDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.musicalDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("music"), new CharsRef(dictionaries.musicalDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.politicsDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("politic"), new CharsRef(dictionaries.politicsDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.religionDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("religion"), new CharsRef(dictionaries.religionDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.road_tripsDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("road"), new CharsRef(dictionaries.road_tripsDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.romanceDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("romance"), new CharsRef(dictionaries.romanceDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.schoolDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("school"), new CharsRef(dictionaries.schoolDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.sportsDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("sport"), new CharsRef(dictionaries.sportsDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.super_heroesDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("super heroes"), new CharsRef(dictionaries.super_heroesDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.travelDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("travel"), new CharsRef(dictionaries.travelDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.workDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("work"), new CharsRef(dictionaries.workDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.electionsDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("election"), new CharsRef(dictionaries.electionsDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.gun_controlDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("gun control"), new CharsRef(dictionaries.gun_controlDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.mobbingDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("mobbing"), new CharsRef(dictionaries.mobbingDic[key]), true);
+            }
+            for (int i = 1; i <= dictionaries.patriotismDic.Count(); i++)
+            {
+                var key = $"key{i}";
+
+                sBuilder.Add(new CharsRef("patriotism"), new CharsRef(dictionaries.patriotismDic[key]), true);
+            }
+            
             var synonyms = sBuilder.Build();
             return synonyms;
         }
